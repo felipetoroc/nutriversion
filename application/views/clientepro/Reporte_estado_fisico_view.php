0f2a062d683bdb14d4efd555133b5da1283939e7
elirviktor <31961327+elirviktor@users.noreply.github.com>
2017-11-20 01:25:39 -0300

    reporte_listo

--- application/views/clientepro/Reporte_estado_fisico_view.php
+++ application/views/clientepro/Reporte_estado_fisico_view.php
@@ -1,3 +1,3 @@
 
 
-<iframe width="800" height="600" src="https://app.powerbi.com/view?r=eyJrIjoiM2M0ZWNjYWEtNjA4MS00Zjc5LTg4OWMtZmE4ZGM2YzM4MzM1IiwidCI6ImU0NmE0MzhlLWNkODctNGRlMS1hYjk1LWU4NmUwNjIzNDZkZCIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
+<iframe width="100%" height="600" src="https://app.powerbi.com/view?r=eyJrIjoiM2M0ZWNjYWEtNjA4MS00Zjc5LTg4OWMtZmE4ZGM2YzM4MzM1IiwidCI6ImU0NmE0MzhlLWNkODctNGRlMS1hYjk1LWU4NmUwNjIzNDZkZCIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
--- application/views/clientepro/navegacion.php
+++ application/views/clientepro/navegacion.php
@@ -1,3 +1,4 @@
 <li><a href="<?php echo base_url() ?>index.php/clientepro"> Inicio</a></li>
 <li><a href='<?php echo base_url() ?>index.php/clientepro/dietas'>Dietas</a></li>
 <li><a href='<?php echo base_url() ?>index.php/clientepro/clientes'>Clientes</a></li>
+<li><a href='<?php echo base_url() ?>index.php/clientepro/ReporteEstadoFisico'>Reportes</a></li>
