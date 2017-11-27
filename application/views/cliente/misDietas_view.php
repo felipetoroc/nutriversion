c517d63d7456324303776a95ea2ec2f942f6e7ab
arecher26 <33816142+arecher26@users.noreply.github.com>
2017-11-27 01:27:08 -0300

    commit

--- application/views/cliente/misDietas_view.php
+++ application/views/cliente/misDietas_view.php
@@ -62,23 +62,91 @@
     <?php       
         if($cumplimiento == 0 )
         { ?>
-            <img src="<?=base_url()."img/medidor-11.png" ?>" width="80%" height="80%">
+            <img src="<?=base_url()."img/medidor--11.png" ?>" width="80%" height="80%">
         <?php }
         if($cumplimiento > 0 && $cumplimiento <= 0.40)
         { ?>
-            <img src="<?=base_url()."img/medidor-12.png" ?>" width="80%" height="80%">
+            <img src="<?=base_url()."img/medidor--12.png" ?>" width="80%" height="80%">
         <?php }
         if($cumplimiento > 0.40  && $cumplimiento <= 0.80)
         { ?>
-            <img src="<?=base_url()."img/medidor-13.png" ?>" width="80%" height="80%">
+            <img src="<?=base_url()."img/medidor--13.png" ?>" width="80%" height="80%">
         <?php }
         if($cumplimiento > 0.80 && $cumplimiento < 1)
         { ?>
-            <img src="<?=base_url()."img/medidor-14.png" ?>" width="80%" height="80%">
+            <img src="<?=base_url()."img/medidor--14.png" ?>" width="80%" height="80%">
         <?php }
         if($cumplimiento == 1)
         { ?>
-            <img src="<?=base_url()."img/medidor-15.png" ?>" width="80%" height="80%">
+            <img src="<?=base_url()."img/medidor--15.png" ?>" width="80%" height="80%">
+        <?php }
+        if($cumplimiento > 0 && $cumplimiento <= 0.40)
+        { ?>
+            <img src="<?=base_url()."img/medidor--16.png" ?>" width="80%" height="80%">
+        <?php }
+        if($cumplimiento > 0 && $cumplimiento <= 0.40)
+        { ?>
+            <img src="<?=base_url()."img/medidor--17.png" ?>" width="80%" height="80%">
+        <?php }
+        if($cumplimiento > 0 && $cumplimiento <= 0.40)
+        { ?>
+            <img src="<?=base_url()."img/medidor--18.png" ?>" width="80%" height="80%">
+        <?php }
+        if($cumplimiento > 0 && $cumplimiento <= 0.40)
+        { ?>
+            <img src="<?=base_url()."img/medidor--19.png" ?>" width="80%" height="80%">
+        <?php }
+        if($cumplimiento > 0 && $cumplimiento <= 0.40)
+        { ?>
+            <img src="<?=base_url()."img/medidor--20.png" ?>" width="80%" height="80%">
+        <?php }
+        if($cumplimiento > 0 && $cumplimiento <= 0.40)
+        { ?>
+            <img src="<?=base_url()."img/medidor--21.png" ?>" width="80%" height="80%">
+        <?php }
+        if($cumplimiento > 0 && $cumplimiento <= 0.40)
+        { ?>
+            <img src="<?=base_url()."img/medidor-22.png" ?>" width="80%" height="80%">
+        <?php }
+        if($cumplimiento > 0 && $cumplimiento <= 0.40)
+        { ?>
+            <img src="<?=base_url()."img/medidor--23.png" ?>" width="80%" height="80%">
+        <?php }
+        if($cumplimiento > 0 && $cumplimiento <= 0.40)
+        { ?>
+            <img src="<?=base_url()."img/medidor--24.png" ?>" width="80%" height="80%">
+        <?php }
+        if($cumplimiento > 0 && $cumplimiento <= 0.40)
+        { ?>
+            <img src="<?=base_url()."img/medidor--25.png" ?>" width="80%" height="80%">
+        <?php }
+        if($cumplimiento > 0 && $cumplimiento <= 0.40)
+        { ?>
+            <img src="<?=base_url()."img/medidor-26.png" ?>" width="80%" height="80%">
+        <?php }
+        if($cumplimiento > 0 && $cumplimiento <= 0.40)
+        { ?>
+            <img src="<?=base_url()."img/medidor--27.png" ?>" width="80%" height="80%">
+        <?php }
+        if($cumplimiento > 0 && $cumplimiento <= 0.40)
+        { ?>
+            <img src="<?=base_url()."img/medidor--23.png" ?>" width="80%" height="80%">
+        <?php }
+        if($cumplimiento > 0 && $cumplimiento <= 0.40)
+        { ?>
+            <img src="<?=base_url()."img/medidor--28.png" ?>" width="80%" height="80%">
+        <?php }
+        if($cumplimiento > 0 && $cumplimiento <= 0.40)
+        { ?>
+            <img src="<?=base_url()."img/medidor--29.png" ?>" width="80%" height="80%">
+        <?php }
+        if($cumplimiento > 0 && $cumplimiento <= 0.40)
+        { ?>
+            <img src="<?=base_url()."img/medidor--31.png" ?>" width="80%" height="80%">
+        <?php }
+         if($cumplimiento > 0 && $cumplimiento <= 0.40)
+        { ?>
+            <img src="<?=base_url()."img/medidor--30.png" ?>" width="80%" height="80%">
         <?php }
         ?>
 </p>   
