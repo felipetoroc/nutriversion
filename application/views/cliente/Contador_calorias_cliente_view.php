9c504258ccad0653d17769b4e6bddc0dd4f473c7
felipetoroc <felipetoro.c@gmail.com>
2017-11-30 02:20:26 -0300

    vovler a hoy

--- application/views/cliente/Contador_calorias_cliente_view.php
+++ application/views/cliente/Contador_calorias_cliente_view.php
@@ -7,7 +7,7 @@
                     <input id="fecha_detalle" type="text" name="bday" placeholder="Mostrar Alimentos Consumidos el...">
                 </div>
                 <div class="large-6 columns">
-                    <input id="btnMostrarTodo" type="button" class="button tiny warning" value="Mostrar Todo">
+                    <input id="btnMostrarTodo" type="button" class="button tiny warning" value="Volver a Hoy">
                 </div>
             </div>
         </div>
